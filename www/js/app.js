3ac53dce8f96682a524c93748b48b52751db9813
lasayter <lasayter@outlook.com>
2016-03-16 21:44:19 +0100

    Refactoring.

--- www/js/app.js
+++ www/js/app.js
@@ -3,49 +3,37 @@
 (function (window) {
 
     // Trade System BOSS.
-    window.tradeSystemSlug = 'binary-options-signals-source';
+    var tradeSystemSlug = 'binary-options-signals-source';
     // BOSS provides trade signals signals every 5 minutes.
-    window.tradeSystemInverval = 60000;
+    var tradeSystemInverval = 60000;
     // Symbols dictionary.
-    window.symbols = {};
+    var symbols = {};
     // Signals table object for easy data manipulation.
-    window.signalsTable = null;
+    var symbolsTable = null;
     // Max loaded symbol's ID.
-    window.maxSignalId = 0;
+    var maxSignalId = 0;
     // Max loaded signal time (timestamp in milliseconds)
     var maxSignalTime = 0;
     // Max delay for server response retrieving
     var maxServerDelay = 2000;
     // Max signals count. We should not store old or expired signals.
-    window.maxSignalsCount = 10;
-
-    var loadedSignalsCount = 0;
+    var maxSignalsCount = 10;
 
     var loadSignal = function(signal){
-        if (updateMaxSignalId(signal.id) < window.maxSignalId && loadedSignalsCount >= window.maxSignalsCount) {
-            // console.log('Signal #' + signal.id + ' skipped');
+        if (signal.id <= maxSignalId) {
             return false;
         }
-        loadedSignalsCount++;
-        updateMaxSignalTime(signal);
-        window.signalsTable.addSignal(signal);
-    };
-
-    var updateMaxSignalId = function (value) {
-        window.maxSignalId = Math.max(value, window.maxSignalId);
-        return value;
-    };
-
-    var updateMaxSignalTime = function(signal){
+        maxSignalId = Math.max(signal.id, maxSignalId);
         maxSignalTime = Math.max(signal.tsMs, maxSignalTime);
+        symbolsTable.addSignal(signal);
     };
 
     var initSignalsTable = function(signals){
-        window.signalsTable = ReactDOM.render(
+        symbolsTable = ReactDOM.render(
             React.createElement(SignalsTable, {
                 signals: signals
-                , interval: window.tradeSystemInverval
-                , limit: window.maxSignalsCount
+                , interval: tradeSystemInverval
+                , limit: maxSignalsCount
             }),
             window.document.getElementById('signals-table-container')
         );
@@ -63,22 +51,28 @@
     initSignalsTable([]);
 
     var feed = function() {
-        client.signals.read( window.tradeSystemSlug ).done(function(data) {
+        client.signals.read( tradeSystemSlug ).done(function(data) {
+            data.signals.sort(function (a, b) {
+                if (a.id < b.id) {
+                    return -1;
+                } else if (a.id > b.id) {
+                    return 1;
+                }
+                return 0;
+            });
             for (var i in data.signals) {
                 var signal = data.signals[i];
-                var symbolId = signal.symbol_id;
-                var direction = (signal.direction == 'Up') ? 'Call' : 'Put';
                 loadSignal({
                     key: signal.id
                     , id: signal.id
-                    , symbol: window.symbols[symbolId].name
-                    , direction: direction
+                    , symbol: symbols[signal.symbol_id].name
+                    , direction: (signal.direction == 'Up') ? 'Call' : 'Put'
                     , time: signal.created_at.substr(11, 8)
                     , reliability: signal.reliability
                     , tsMs: +new Date(signal.created_at) - (new Date).getTimezoneOffset() * 1000 * 60
                 });
             }
-            setTimeout(feed, maxSignalTime + window.tradeSystemInverval -new Date() + maxServerDelay);
+            setTimeout(feed, maxSignalTime + tradeSystemInverval -new Date() + maxServerDelay);
         });
     };
 
